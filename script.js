01e160910d0523db2b053990c36213948e943da3
RajatArya007zxc <rajatarya199zx@gmail.com>
2023-12-20 23:43:34 +0530

    update

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Find the form element with the specified class
     var formElement = document.querySelector('.product-single__form.uploadkit-form');
-
+  console.log("Running and get form ELEMENT",formElement)
     // Check if the form element exists
     if (formElement) {
-        // Find the button element with the specified class within the form
-        var buttonElement = formElement.querySelector('.btn.button');
+        console.log(" get form ELEMENT 🆎",formElement)
+        // Create a new button element
+        var newButton = document.createElement('button');
+        newButton.id = 'custom_designButton';
+        newButton.style.backgroundColor = '#fad71e';
+        newButton.style.color = 'black';
+        newButton.style.padding = '10px 30px';
+        newButton.style.fontSize = '18px';
+        newButton.style.fontWeight = '700';
+        newButton.textContent = 'Create design';
 
-        // Check if the button element exists
-        if (buttonElement) {
-            // Add a click event listener to the button
-            buttonElement.addEventListener('click', function () {
-                // Get the current URL
-                var currentUrl = window.location.href;
+        // Replace the form element with the new button
+        formElement.parentNode.replaceChild(newButton, formElement);
 
-                // Extract the variant ID from the URL
-                var match = currentUrl.match(/variant=(\d+)/);
-                var variantId = match ? match[1] : null;
+        // Add a click event listener to the new button
+        newButton.addEventListener('click', function () {
+            console.log("action on new button",newButton)
+            // Get the current URL
+            var currentUrl = window.location.href;
 
-                // Check if the variant ID is found
-                if (variantId) {
-                    // Redirect to the specified URL with the variant ID
-                    var redirectUrl = 'http://localhost:3000?variant=' + variantId;
-                    window.location.href = redirectUrl;
-                } else {
-                    console.error('Variant ID not found in the URL.');
-                }
-            });
-        }
+            // Extract the variant ID from the URL
+            var match = currentUrl.match(/variant=(\d+)/);
+            var variantId = match ? match[1] : null;
+            console.log("current URL ",currentUrl)
+            // Check if the variant ID is found
+            if (variantId) {
+                // Redirect to the specified URL with the variant ID
+                var redirectUrl = 'http://localhost:3000?variant=' + variantId;
+                window.location.href = redirectUrl;
+            } else {
+                console.error('Variant ID not found in the URL.');
+            }
+        });
     }
 });
